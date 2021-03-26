7647b09f68e1c736cf5930be5e0c8fc103328d60
Dmitry Simonenko <dmitry.simonenko@gmail.com>
2021-03-26 17:57:53 +0300

    lesson 2

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type action struct {
 	name string
@@ -8,35 +11,42 @@ type action struct {
 }
 
 func menu(actions []action) {
+	var choice int
 
 	for {
+		choice = 0
+
 		fmt.Println("Действия:")
 		for c, a := range actions {
 			fmt.Printf("%d: %s\n", c+1, a.name)
 		}
 		fmt.Print("Введите номер действия (0 - выход): ")
-		choose := 0
-		_, err := fmt.Scan(&choose)
+		n, err := fmt.Scanln(&choice)
 		if err != nil {
 			fmt.Printf("Ошибка: %v\n", err)
 			continue
 		}
-		if choose == 0 {
+		if n == 0 {
+			fmt.Println("Ошибка: не выбрано действие")
+			return
+		}
+
+		if choice == 0 {
 			break
 		}
-
-		if choose > len(actions) {
-			fmt.Printf("Ошибка: действие с номером %d отсутствует\n", choose)
+		if choice > len(actions) {
+			fmt.Printf("Ошибка: действие с номером %d отсутствует\n", choice)
 		} else {
-			actions[choose-1].do()
+			actions[choice-1].do()
 		}
 	}
-
 }
 
 func main() {
 	actions := []action{
 		{"Площадь прямоугольника", square},
+		{"Диаметр круга и длина окружности", circle},
+		{"Разряды числа", digits},
 	}
 	menu(actions)
 }
@@ -67,4 +77,53 @@ func square() {
 	}
 
 	fmt.Printf("Площать прямоугольника: %d (x=%d y=%d)\n", x*y, x, y) //
+}
+
+func circle() {
+	var sq float64
+	fmt.Println("Действие: расчет диаметра круга и длины окружности")
+	fmt.Print("Введите площадь круга: ")
+	n, err := fmt.Scanln(&sq)
+	if err != nil {
+		fmt.Printf("Ошибка: %v\n", err)
+		return
+	}
+	if n == 0 {
+		fmt.Println("Ошибка: не введено значение площади круга")
+		return
+	}
+
+	radius := math.Sqrt(sq / math.Pi)
+	fmt.Printf("Диаметр круга: %f\n", radius*2)
+	fmt.Printf("Длина окруности: %f\n", 2*math.Pi*radius)
+}
+
+func digits() {
+	var number int
+
+	fmt.Println("Действие: разряды числа")
+	fmt.Print("Введите число от 0 до 999: ")
+	n, err := fmt.Scanln(&number)
+
+	if err != nil {
+		fmt.Printf("Ошибка: %v\n", err)
+		return
+	}
+	if n == 0 {
+		fmt.Println("Ошибка: не введено число")
+		return
+	}
+
+	if number > 999 {
+		fmt.Println("Ошибка: введено слишком большое число")
+		return
+	}
+
+	hundreds := number / 100
+	fmt.Printf("Сотни: %d\n", hundreds)
+	number = number % 100
+
+	dozens := number / 10
+	fmt.Printf("Десятки: %d\n", dozens)
+	fmt.Printf("Единицы: %d\n", number%10)
 }
