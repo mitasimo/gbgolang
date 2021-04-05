03c58ea5d0f93f8f1ab49aced85f6e8b430388eb
Dmitry Simonenko <dmitry.simonenko@gmail.com>
2021-04-05 14:56:10 +0300

    lesson 3

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 )
 
 func main() {
+	const (
+		ErrSecondOperandIsZero = "Ошибка: второй операнд не может быть равен ноль (0)"
+	)
+
 	var (
 		first, second, result int
 		operator              string
@@ -12,22 +16,23 @@ func main() {
 	)
 
 	for {
+		fmt.Println()
 		fmt.Print("Введите значение первого операнда: ")
 		_, err = fmt.Scanln(&first)
 		if err != nil {
-			fmt.Printf("Ошибка ввода первого операнда: %v\n", err)
+			fmt.Printf("\nОшибка ввода первого операнда: %v\n", err)
 			continue
 		}
 		fmt.Print("Введите значение второго операнда: ")
 		_, err = fmt.Scanln(&second)
 		if err != nil {
-			fmt.Printf("Ошибка ввода второго операнда: %v\n", err)
+			fmt.Printf("\nОшибка ввода второго операнда: %v\n", err)
 			continue
 		}
 		fmt.Print("Введите оператор (+, -, *, /, %): ")
 		_, err = fmt.Scanln(&operator)
 		if err != nil {
-			fmt.Printf("Ошибка ввода оператора: %v\n", err)
+			fmt.Printf("\nОшибка ввода оператора: %v\n", err)
 			continue
 		}
 
@@ -40,13 +45,13 @@ func main() {
 			result = first * second
 		case "/":
 			if second == 0.0 {
-				fmt.Println("Ошибка: второй оперант не может быть равен ноль (0)")
+				fmt.Println(ErrSecondOperandIsZero)
 				continue
 			}
 			result = first / second
 		case "%":
 			if second == 0.0 {
-				fmt.Println("Ошибка: второй оперант не может быть равен ноль (0)")
+				fmt.Println(ErrSecondOperandIsZero)
 				continue
 			}
 			result = first % second
@@ -54,6 +59,6 @@ func main() {
 			fmt.Printf("Ошибка: оператор %s не поддерживается\n", operator)
 			continue
 		}
-		fmt.Printf("Результат %d %s %d = %d\n\n", first, operator, second, result)
+		fmt.Printf("Результат %d %s %d = %d\n", first, operator, second, result)
 	}
 }
