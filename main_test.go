5308114752d6666bf3b81259f2ca81a1180de7cc
Dmitry Simonenko <dmitry.simonenko@gmail.com>
2021-04-06 16:13:01 +0300

    Добавлены тесты сортировки вставками

--- main_test.go
+++ main_test.go
@@ -1,8 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func Test_quickSort(t *testing.T) {
+func TestSortQuick(t *testing.T) {
 	tests := []struct {
 		name          string
 		input, sorted []int
@@ -20,10 +22,38 @@ func Test_quickSort(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			quickSort(tt.input)
+			SortQuick(tt.input)
 			for i, v := range tt.input {
 				if v != tt.sorted[i] {
-					t.Fatalf("Array %v after sorting must be %v", tt.input, tt.sorted)
+					t.Fatalf("%s: array %v after sorting must be %v", tt.name, tt.input, tt.sorted)
+				}
+			}
+		})
+	}
+}
+
+func TestSortInsert(t *testing.T) {
+	tests := []struct {
+		name          string
+		input, sorted []int
+	}{
+		{
+			name:   "simple",
+			input:  []int{2, 4, 7, 9, 0, 5, 1},
+			sorted: []int{0, 1, 2, 4, 5, 7, 9},
+		},
+		{
+			name:   "one",
+			input:  []int{2},
+			sorted: []int{2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			SortInsert(tt.input)
+			for i, v := range tt.input {
+				if v != tt.sorted[i] {
+					t.Fatalf("%s: array %v after sorting must be %v", tt.name, tt.input, tt.sorted)
 				}
 			}
 		})
