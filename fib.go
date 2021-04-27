8a0ef395a217c73f7fe7ecf29a3bfc2acb4734d4
Dmitry Simonenko <dmitry.simonenko@gmail.com>
2021-04-27 13:59:42 +0300

    lesson 10

---
+++ fib.go
@@ -0,0 +1,36 @@
+package main
+
+func FibVars(n int) int {
+	if n < 2 {
+		return n
+	}
+
+	pred, cur := 0, 1
+	for i := 2; i <= n; i++ {
+		cur, pred = pred+cur, cur
+	}
+
+	return cur
+}
+
+func FibCache(n int) int {
+	if n < 2 {
+		return n
+	}
+
+	cache := make([]int, n+1)
+	cache[0], cache[1] = 0, 1
+	for i := 2; i <= n; i++ {
+		cache[i] = cache[i-1] + cache[i-2]
+	}
+
+	return cache[n]
+}
+
+func FibRecur(n int) int {
+	if n < 2 {
+		return n
+	}
+
+	return FibRecur(n-2) + FibRecur(n-1)
+}
---
+++ fib_test.go
@@ -0,0 +1,69 @@
+package main
+
+import "testing"
+
+func TestFibVars(t *testing.T) {
+	insideTestFib(t, FibVars, "FibVars")
+}
+
+func TestFibCache(t *testing.T) {
+	insideTestFib(t, FibCache, "FibCache")
+}
+
+func TestFibRecur(t *testing.T) {
+	insideTestFib(t, FibRecur, "FibRecur")
+}
+
+func insideTestFib(t *testing.T, f func(int) int, fn string) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantF   int
+		wantErr bool
+	}{
+		{"-2", args{-2}, -2, true},
+		{"0", args{0}, 0, false},
+		{"1", args{1}, 1, false},
+		{"2", args{2}, 1, false},
+		{"53", args{30}, 832040, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotF := f(tt.args.n)
+			if gotF != tt.wantF {
+				t.Errorf("%s() = %v, want %v", fn, gotF, tt.wantF)
+			}
+		})
+	}
+}
+
+var globalF int
+
+func BenchmarkFibVars(t *testing.B) {
+	var f int
+	for i := 0; i <= t.N; i++ {
+		f = FibVars(30)
+	}
+
+	globalF = f
+}
+func BenchmarkFibCache(t *testing.B) {
+	var f int
+	for i := 0; i <= t.N; i++ {
+		f = FibCache(30)
+	}
+
+	globalF = f
+}
+
+func BenchmarkFibRecur(t *testing.B) {
+	var f int
+	for i := 0; i <= t.N; i++ {
+		f = FibRecur(30)
+	}
+
+	globalF = f
+}
