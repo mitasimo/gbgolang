136a9f2c60b3083e7a4d2148f767008b23f5fc54
Dmitry Simonenko <dmitry.simonenko@gmail.com>
2021-04-27 16:58:12 +0300

    lesson 10

--- main.go
+++ main.go
@@ -9,12 +9,13 @@ import (
 func main() {
 
 	const (
-		n  = 500
+		n  = 10000
 		ln = 30
 	)
 
 	fmt.Printf("FibVars(%d)\t= %d\n", n, fibonachi.FibVars(n))
-	fmt.Printf("FibCache(%d)\t= %d\n", n, fibonachi.FibCache(n))
+	fmt.Printf("FibSlice(%d)\t= %d\n", n, fibonachi.FibSlice(n))
+	fmt.Printf("FibMap(%d)\t= %d\n", n, fibonachi.FibMap(n))
 	fmt.Printf("FibRecur(%d)\t= %d\n", ln, fibonachi.FibRecur(ln))
 
 }
